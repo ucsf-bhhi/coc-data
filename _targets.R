dc81918a1dac2da1c722aff5bfb96caf003685ac
Eve Perry <eve.perry@ucsf.edu>
2021-05-26 12:15:28 -0700

    add processing functions to targets

--- _targets.R
+++ _targets.R
@@ -4,39 +4,54 @@ library(future)
 library(future.callr)
 plan(callr)
 source("code/helpers.R")
+source("code/process_coc_shapefiles.R")
 source("code/coc_county_tract_crosswalk.R")
 tar_option_set(packages = c("tidyverse", "sf", "rmapshaper", "tidycensus", "fs"))
 
 list(
-  tar_target(shapefile_year, 2013:2019),
+  tar_files_input(
+    raw_coc_shapefiles,
+    fs::dir_ls("input_data/geography/coc_shapefiles", recurse = 1, regex = build_regex()),
+    format = "file"
+  ),
   tar_target(
-    coc_shapefile,
-    load_coc_shapefile(shapefile_year, "simplified", "output_data/coc_shapefiles"),
-    pattern = map(shapefile_year),
+    shapefile_years,
+    parse_number(raw_coc_shapefiles)
+  ),
+  tar_target(
+    original_coc_shapefiles,
+    read_raw_coc_shapefile(raw_coc_shapefiles),
+    pattern = map(raw_coc_shapefiles),
     iteration = "list"
   ),
   tar_target(
-    coc_dissolved, 
-    load_coc_shapefile(shapefile_year, "dissolved_simplified", "output_data/coc_shapefiles"),
-    pattern = map(shapefile_year),
+    simplified_coc_shapefiles, 
+    simplify_shapefile(original_coc_shapefiles),
+    pattern = map(original_coc_shapefiles),
+    iteration = "list"
+  ),
+  tar_target(
+    dissolved_coc_shapefiles, 
+    ms_dissolve(simplified_coc_shapefiles, copy_fields = "year"),
+    pattern = map(simplified_coc_shapefiles),
     iteration = "list"
   ),
   tar_target(
     tracts, 
-    fetch_tract_data(shapefile_year, crs = 2163),
-    pattern = map(shapefile_year),
+    fetch_tract_data(shapefile_years, crs = 2163),
+    pattern = map(shapefile_years),
     iteration = "list"
   ),
   tar_target(
     clipped_tracts, 
-    clip_tracts(tracts, coc_dissolved),
-    pattern = map(tracts, coc_dissolved),
+    clip_tracts(tracts, dissolved_coc_shapefiles),
+    pattern = map(tracts, dissolved_coc_shapefiles),
     iteration = "list"
   ),
   tar_target(
     tract_cocs, 
-    map_tracts_to_cocs(clipped_tracts, coc_shapefile),
-    pattern = map(clipped_tracts, coc_shapefile),
+    map_tracts_to_cocs(clipped_tracts, simplified_coc_shapefiles),
+    pattern = map(clipped_tracts, simplified_coc_shapefiles),
     iteration = "list"
   ),
   tar_target(
@@ -51,8 +66,12 @@ list(
   ),
   tar_target(
     save_tract_crosswalk, 
-    write_crosswalk(tract_crosswalk, shapefile_year, "tract", output_directory = "output_data/"),
+    write_crosswalk(tract_crosswalk, "tract", output_directory = "output_data/"),
     format = "file"
   ),
-  tar_target(save_county_crosswalk, write_crosswalk(county_crosswalk, shapefile_year, "county", output_directory = "output_data/"), format = "file")
+  tar_target(
+    save_county_crosswalk,
+    write_crosswalk(county_crosswalk, "county", output_directory = "output_data/"),
+    format = "file"
+  )
 )
