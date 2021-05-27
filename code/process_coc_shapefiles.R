9e3eb35997ad4541b71373a2ffc5fbd0a84cba5e
Eve Perry <eve.perry@ucsf.edu>
2021-05-27 12:46:55 -0700

    add process_coc_shapefiles.R docs

--- code/process_coc_shapefiles.R
+++ code/process_coc_shapefiles.R
@@ -1,17 +1,51 @@
-build_regex = function(file_extensions = c("gdb", "shp")) {
+#' Build filename regular expression
+#'
+#' Creates a regular expression used to filter shapefile in input_data
+#'
+#' @param file_extensions A character vector of shapefile file extensions to
+#'   include in the regular expression.
+#'
+#' @return A character vector containing the regular expression.
+build_regex <- function(file_extensions = c("gdb", "shp")) {
   paste0("[.](", paste(file_extensions, collapse = "|"), ")$")
 }
 
-read_raw_coc_shapefile = function(shapefile_path) {
-  shapefile_year = parse_number(shapefile_path)
+
+#' Read in original CoC shapefile
+#'
+#' Loads an original CoC shapefile from HUD as a spatial data frame and adds a
+#' year column. Returns an error if it cannot parse a 4-digit year from the file
+#' path.
+#' @param shapefile_path Path to the shapefile or other spatial data
+#'   file/database
+#'
+#' @return The shapefile in a spatial data frame
+read_raw_coc_shapefile <- function(shapefile_path) {
+  # try to parse a year from the shapefile path
+  shapefile_year <- parse_number(shapefile_path)
+  # throw an error if we don't have a 4 digit number for the year
   stopifnot("coc shapefile year not 4 digits" = nchar(as.character(shapefile_year)) == 4)
-  st_read(shapefile_path) %>% 
+  # read in the shapefile
+  st_read(shapefile_path) %>%
+    # add a column with the year
     mutate(year = shapefile_year)
 }
 
-simplify_shapefile = function(shapefile, projected_crs = 2163, simplify_keep_pct = 0.1) {
-  st_transform(shapefile, crs = projected_crs) %>% 
+
+#' Simplify shapefile and set projected CRS
+#'
+#' Simplifies a shapefile using [rmapshaper::ms_simplify()] and also sets a
+#' projected CRS using [sf::st_transform()] since [rmapshaper::ms_simplify()]
+#' prefers a projected CRS.
+#'
+#' @param shapefile An sf object
+#' @param projected_crs The EPSG code for the projected CRS
+#' @param simplify_keep_pct The share of the shapefile to keep when simplifying
+#'
+#' @return The simplified sf object now in the projected CRS.
+simplify_shapefile <- function(shapefile, projected_crs = 2163, simplify_keep_pct = 0.1) {
+  # transform to the projected CRS
+  st_transform(shapefile, crs = projected_crs) %>%
+    # simplify the spatial object
     ms_simplify(keep = simplify_keep_pct)
 }
-
-
