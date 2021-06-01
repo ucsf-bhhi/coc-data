2070de595f787e9a85adde443db0e269c44eb638
Eve Perry <eve.perry@ucsf.edu>
2021-06-01 13:15:07 -0700

    targetize pit rates (#16)
    * convert pit_rates to targets
    
    * add documentation

--- code/pit_rates.R
+++ code/pit_rates.R
@@ -1,48 +1,34 @@
-library(tidyverse)
-source("code/helpers.R")
-
-acs_start_year <- 2012
-acs_end_year <- 2019
-
-input_dir <- "input_data"
-output_dir <- "output_data"
-
-
-# read in the processed PIT data if the object doesn't already exist in the environment
-if (!exists("pit_data")) {
-  pit_data <- read_csv(file.path(output_dir, "pit_processed_2007_2019.csv"))
+get_overall_pit_counts <- function(pit_data) {
+  pit_data %>%
+    filter(category == "Overall Homeless") %>%
+    select(coc_number, year, overall_homeless = count)
 }
 
-# just grab the overall homelessness count
-pit_overall <- pit_data %>%
-  filter(category == "Overall Homeless") %>%
-  select(coc_number, year, overall_homeless = count)
-
-# if the augmented county to coc crosswalk doesn't exist, run the code to produce it
-if (!file.exists("output_data/coc_population.csv")) {
-  source("code/coc_population.R")
+make_pit_rates <- function(pit_data, coc_populations) {
+  # start with the CoC population data
+  coc_populations %>%
+    # join on the homelessness counts from the PIT data
+    left_join(pit_data, by = c("coc_number", "year")) %>%
+    # calculate the homelessness rate in the total population and population in poverty
+    mutate(
+      homeless_rate_total_pop = overall_homeless / coc_pop,
+      homeless_rate_in_poverty = overall_homeless / coc_poverty_pop,
+      # also calculate the number of people experiencing homelessness per 1000 people in the total population and population in poverty
+      homeless_per_1000_total_pop = homeless_rate_total_pop * 1000,
+      homeless_per_1000_in_poverty = homeless_rate_in_poverty * 1000
+    ) %>%
+    select(coc_number, coc_name, year, overall_homeless, homeless_rate_total_pop, homeless_rate_in_poverty, homeless_per_1000_total_pop, homeless_per_1000_in_poverty)
 }
-# read in the CoC population data
-coc_population <- read_csv("output_data/coc_population.csv")
 
-# combine the data and calculate homelessness rates
-combined_data <- coc_population %>%
-  # join on the homelessness counts from the PIT data
-  left_join(pit_overall, by = c("coc_number", "year")) %>%
-  # calculate the homelessness rate in the total population and population in poverty
-  mutate(
-    homeless_rate_total_pop = overall_homeless / coc_pop,
-    homeless_rate_in_poverty = overall_homeless / coc_poverty_pop,
-    # also calculate the number of people experiencing homelessness per 1000 people in the total population and population in poverty
-    homeless_per_1000_total_pop = homeless_rate_total_pop * 1000,
-    homeless_per_1000_in_poverty = homeless_rate_in_poverty * 1000
-  ) %>%
-  select(coc_number, coc_name, year, overall_homeless, homeless_rate_total_pop, homeless_rate_in_poverty, homeless_per_1000_total_pop, homeless_per_1000_in_poverty)
-
-# set homelessness rates output file name
-output_name <- paste0("pit_homelessness_rates_", acs_start_year, "_", acs_end_year, ".csv")
-# set the output file path
-output_path <- file.path(output_dir, output_name)
-
-# write csv
-write_csv(combined_data, output_path)
+#' Calculate homelessness rates by CoC
+#'
+#' @param pit_data A data frame with PIT counts in long form
+#' @param coc_populations A data frame with CoC overall populations and
+#'   populations in poverty
+#'
+#' @return A data frame with CoC homelessness counts, rates, and number of
+#'   unhoused people per 1000 people in the overall population.
+build_pit_rates <- function(pit_data, coc_populations) {
+  get_overall_pit_counts(pit_data) %>%
+    make_pit_rates(coc_populations)
+}
