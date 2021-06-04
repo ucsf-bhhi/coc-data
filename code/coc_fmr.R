4bfd199d480c24b6c69eb853e1b0276f14aa530f
Eve Perry <eve.perry@ucsf.edu>
2021-06-04 15:51:33 -0700

    style

--- code/coc_fmr.R
+++ code/coc_fmr.R
@@ -2,8 +2,8 @@
 #'
 #' Reads in the raw FMR spreadsheets, standardizes the FIPS code and FMR
 #' variables, removes unneeded variables, and counts the number of records per
-#' county. 
-#' 
+#' county.
+#'
 #' Counting the records per county is necessary because unlike the rest
 #' of the U.S, which uses groups of counties for FMR areas, New England states
 #' create FMR areas with county subdivisions, so a county can be in more than
@@ -19,30 +19,30 @@ process_fmr <- function(raw_fmr_path) {
     rename_with(str_to_lower, everything()) %>%
     # add a column with the year of the data which we get from parsing the filepath
     mutate(year = parse_number(raw_fmr_path)) %>%
-    select(year, starts_with("fips"), starts_with("fmr"), -ends_with("chg"), -matches("fips2000"), state, county, metro_code, areaname, metro) %>% 
+    select(year, starts_with("fips"), starts_with("fmr"), -ends_with("chg"), -matches("fips2000"), state, county, metro_code, areaname, metro) %>%
     # sometimes the FIPS code is called fips and sometimes fips2010 so
     # standardize it as fips10
-    rename_with(~ "fips10", starts_with("fips")) %>%  
-    #create a 5-digit version of the fips code that has just state and county
+    rename_with(~"fips10", starts_with("fips")) %>%
+    # create a 5-digit version of the fips code that has just state and county
     mutate(fips5 = str_sub(fips10, 1, 5)) %>%
     # sometimes there's an underscore in the fmr columns so standardize without
     # the underscore
-    rename_with(~ str_remove(.x, "_"), starts_with("fmr")) %>% 
-    mutate(across(starts_with("fmr"), as.numeric)) %>% 
+    rename_with(~ str_remove(.x, "_"), starts_with("fmr")) %>%
+    mutate(across(starts_with("fmr"), as.numeric)) %>%
     select(fips10, year, fmr0, fmr1, fmr2, fmr3, fmr4, fips5, state, county, metro_code, areaname, metro) %>%
     # count how many records we have per county (some counties are subdivided
     # into multiple fmr areas, we'll deal with that later)
     group_by(year, state, county) %>%
-    mutate(count = n()) %>% 
+    mutate(count = n()) %>%
     ungroup()
 }
 
 #' @describeIn get_acs_county_sub Finds the states with sub-county FMRs
 #'
-#' @param processed_fmr 
+#' @param processed_fmr
 #'
 #' @return
-get_states_with_split_counties = function(processed_fmr) {
+get_states_with_split_counties <- function(processed_fmr) {
   processed_fmr %>%
     filter(count > 1) %>%
     # pops the column out from the data frame and stores it as a vector
@@ -60,28 +60,28 @@ get_states_with_split_counties = function(processed_fmr) {
 #' @param processed_fmr Data frame with processed FMRs
 #'
 #' @return A data frame with renter household counts.
-get_acs_county_sub = function(year, processed_fmr) {
-  states = get_states_with_split_counties(processed_fmr)
-  
-  fetch_acs("county subdivision", variables = c(renting_households = "B25003_003"), year = year, state = states, key = Sys.getenv("CENSUS_API_KEY"), output = "wide", survey = "acs5") %>% 
-    rename(fips10 = fips) 
+get_acs_county_sub <- function(year, processed_fmr) {
+  states <- get_states_with_split_counties(processed_fmr)
+
+  fetch_acs("county subdivision", variables = c(renting_households = "B25003_003"), year = year, state = states, key = Sys.getenv("CENSUS_API_KEY"), output = "wide", survey = "acs5") %>%
+    rename(fips10 = fips)
 }
 
-#' @describeIn build_coc_fmr Convert sub-county FMRs to county FMRs 
-#' 
+#' @describeIn build_coc_fmr Convert sub-county FMRs to county FMRs
+#'
 #' Calculates a weighted average FMR for the counties that are in more than one
 #' FMR region. County subdivisions are unique to an FMR area so we take the
 #' weighted average of FMRs in the county subdivisions to get the county level
-#' FMR. 
-#' 
+#' FMR.
+#'
 #' @param processed_fmr Data frame with processed FMRs
 #' @param acs Data frame with the county subdivision renter household counts
-#' 
+#'
 #' @return
-process_split_county_fmr = function(processed_fmr, acs) {
-  processed_fmr %>% 
-    filter(count > 1) %>% 
-    left_join(acs, by = c("fips10", "year"))  %>%
+process_split_county_fmr <- function(processed_fmr, acs) {
+  processed_fmr %>%
+    filter(count > 1) %>%
+    left_join(acs, by = c("fips10", "year")) %>%
     # there are some fips10 codes that don't have ACS data
     # set those renter household counts to 0 so weighted.mean doesn't return
     # na (na.rm doesn't apply to the weights in weighted.mean for some reason)
@@ -89,7 +89,8 @@ process_split_county_fmr = function(processed_fmr, acs) {
     group_by(fips5, year) %>%
     # calculate the weighted average for each FMR
     summarize(across(starts_with("fmr"), ~ weighted.mean(.x, renting_households, na.rm = TRUE)),
-              .groups = "drop") %>% 
+      .groups = "drop"
+    ) %>%
     ungroup()
 }
 
@@ -103,10 +104,10 @@ process_split_county_fmr = function(processed_fmr, acs) {
 #' @param crosswalk Data frame with the county to CoC crosswalk
 #'
 #' @return A data frame with CoC level FMRs.
-build_coc_fmr = function(processed_fmr, acs, crosswalk) {
-  process_split_county_fmr(processed_fmr, acs) %>% 
-    bind_rows(filter(processed_fmr, count == 1)) %>% 
-    right_join(crosswalk, by = c("fips5" = "county_fips", "year")) %>% 
+build_coc_fmr <- function(processed_fmr, acs, crosswalk) {
+  process_split_county_fmr(processed_fmr, acs) %>%
+    bind_rows(filter(processed_fmr, count == 1)) %>%
+    right_join(crosswalk, by = c("fips5" = "county_fips", "year")) %>%
     group_by(coc_number, year) %>%
     summarise(
       # calculate the weighted average for each FMR
@@ -117,4 +118,3 @@ build_coc_fmr = function(processed_fmr, acs, crosswalk) {
       .groups = "drop"
     )
 }
-
--- tests/data_tests/test-coc_fmr.R
+++ tests/data_tests/test-coc_fmr.R
@@ -1,6 +1,6 @@
 test_that("coc_fmr is valid", {
   tar_load(coc_fmr)
-  
+
   expect_equal(count_na(coc_fmr, coc_number), 0)
   expect_equal(count_na(coc_fmr, year), 0)
   expect_equal(count_na(coc_fmr, avg_fmr0), 0)
@@ -8,17 +8,16 @@ test_that("coc_fmr is valid", {
   expect_equal(count_na(coc_fmr, avg_fmr2), 0)
   expect_equal(count_na(coc_fmr, avg_fmr3), 0)
   expect_equal(count_na(coc_fmr, avg_fmr4), 0)
-  
+
   expect_true(in_range(coc_fmr, avg_fmr0, 100, 10000))
   expect_true(in_range(coc_fmr, avg_fmr1, 100, 10000))
   expect_true(in_range(coc_fmr, avg_fmr2, 100, 10000))
   expect_true(in_range(coc_fmr, avg_fmr3, 100, 10000))
   expect_true(in_range(coc_fmr, avg_fmr4, 100, 10000))
-  
+
   expect_equal(get_value(coc_fmr, avg_fmr0, coc_number == "CA-502", year == 2013), 892)
   expect_equal(get_value(coc_fmr, avg_fmr0, coc_number == "AL-501", year == 2013), 571.11523)
-  
+
   expect_equal(get_value(coc_fmr, avg_fmr0, coc_number == "CA-502", year == 2019), 1409)
   expect_equal(get_value(coc_fmr, avg_fmr0, coc_number == "AL-501", year == 2019), 703.8183)
-  
 })
