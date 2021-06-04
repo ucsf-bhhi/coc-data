0a858eec5b4eece633a3d1222ba4d6340f75f749
Eve Perry <eve.perry@ucsf.edu>
2021-06-04 16:01:40 -0700

    fix tests

--- tests/data_tests/helper-data_tests.R
+++ tests/data_tests/helper-data_tests.R
@@ -1,24 +1,25 @@
 library(targets, warn.conflicts = FALSE)
 library(dplyr, warn.conflicts = FALSE)
+library(assertr, warn.conflicts = FALSE)
 
-count_na = function(data, column) {
-  data %>% 
-    pull({{ column }}) %>% 
-    is.na() %>% 
+count_na <- function(data, column) {
+  data %>%
+    pull({{ column }}) %>%
+    is.na() %>%
     sum()
 }
 
-in_range = function(data, column, min, max) {
-  data %>% 
-    filter(!is.na({{ column }})) %>% 
-    pull({{ column }}) %>% 
-    between(min, max) %>% 
+in_range <- function(data, column, min, max) {
+  data %>%
+    filter(!is.na({{ column }})) %>%
+    pull({{ column }}) %>%
+    between(min, max) %>%
     all()
 }
 
-get_value = function(data, column, ...) {
-  f = quos(...)
-  data %>%   
-    filter(!!!f) %>% 
+get_value <- function(data, column, ...) {
+  f <- quos(...)
+  data %>%
+    filter(!!!f) %>%
     pull({{ column }})
 }
