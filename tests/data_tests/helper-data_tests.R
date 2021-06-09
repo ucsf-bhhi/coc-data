162e26efe6a04ae28be1453f24801b24e30fd790
Eve Perry <eve.perry@ucsf.edu>
2021-06-09 10:45:30 -0700

    check that test column exists

--- tests/data_tests/helper-data_tests.R
+++ tests/data_tests/helper-data_tests.R
@@ -1,6 +1,20 @@
 library(targets, warn.conflicts = FALSE)
 library(dplyr, warn.conflicts = FALSE)
 library(assertr, warn.conflicts = FALSE)
+library(glue, warn.conflicts = FALSE)
+library(rlang, warn.conflicts = FALSE)
+library(crayon, warn.conflicts = FALSE)
+column_exists <- function(data, column) {
+  column_name <- as_name(enquo(column))
+  data_name <- as_name(enquo(data))
+  
+  check = rlang::has_name(quo_squash(data), column_name)
+  
+  if (!check) {
+     msg = glue_col("{red column '{column_name}' not present in '{data_name}'}")
+     stop(msg, call. = FALSE)
+  }
+}
 
 count_na <- function(data, column) {
   data %>%
@@ -18,6 +32,7 @@ in_range <- function(data, column, min, max) {
 }
 
 get_value <- function(data, column, ...) {
+  column_exists({{ data }}, {{ column }})
   f <- quos(...)
   data %>%
     filter(!!!f) %>%
