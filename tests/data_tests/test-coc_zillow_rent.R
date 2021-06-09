40f6692097674a3454f0fcf82788e55b2765dac3
Eve Perry <eve.perry@ucsf.edu>
2021-06-09 13:08:56 -0700

    style

--- tests/data_tests/test-coc_zillow_rent.R
+++ tests/data_tests/test-coc_zillow_rent.R
@@ -10,18 +10,22 @@ test_that("processed_zillow_data is valid", {
   expect_true(all(processed_zillow_data$annual_mean_rent > 0))
 
   # test a zip-year with complete data
-  get_value(processed_zillow_data, annual_mean_rent,
-            year == 2019, zip == "01752") %>% 
+  get_value(
+    processed_zillow_data, annual_mean_rent,
+    year == 2019, zip == "01752"
+  ) %>%
     expect_equal(1690.41667)
   # test a zip-year with some missing data
-  get_value(processed_zillow_data, annual_mean_rent,
-            year == 2014, zip == "94109") %>% 
+  get_value(
+    processed_zillow_data, annual_mean_rent,
+    year == 2014, zip == "94109"
+  ) %>%
     expect_equal(2578.3)
 })
 
 test_that("processed tract to zip is valid", {
   tar_load(tract_to_zip)
-  
+
   tract_to_zip %>%
     expect_no_na(zip) %>%
     expect_no_na(tract_fips) %>%
@@ -29,25 +33,37 @@ test_that("processed tract to zip is valid", {
     expect_no_na(res_ratio) %>%
     expect_between(year, 2014, 2019) %>%
     expect_between(res_ratio, 0, 1)
-  
-  
-    get_value(tract_to_zip, res_ratio,
-              year == 2014, tract_fips == "01001020200", zip == "36067") %>% 
+
+
+  get_value(
+    tract_to_zip, res_ratio,
+    year == 2014, tract_fips == "01001020200", zip == "36067"
+  ) %>%
     expect_equal(0.40947697810)
-    get_value(tract_to_zip, res_ratio,
-              year == 2015, tract_fips == "01001020200", zip == "36067") %>% 
+  get_value(
+    tract_to_zip, res_ratio,
+    year == 2015, tract_fips == "01001020200", zip == "36067"
+  ) %>%
     expect_equal(0.411528150)
-    get_value(tract_to_zip, res_ratio,
-              year == 2016, tract_fips == "01001020200", zip == "36067") %>% 
+  get_value(
+    tract_to_zip, res_ratio,
+    year == 2016, tract_fips == "01001020200", zip == "36067"
+  ) %>%
     expect_equal(0.409111013)
-    get_value(tract_to_zip, res_ratio,
-              year == 2017, tract_fips == "01001020200", zip == "36067") %>% 
+  get_value(
+    tract_to_zip, res_ratio,
+    year == 2017, tract_fips == "01001020200", zip == "36067"
+  ) %>%
     expect_equal(0.971757322)
-    get_value(tract_to_zip, res_ratio,
-              year == 2018, tract_fips == "01001020200", zip == "36067") %>% 
+  get_value(
+    tract_to_zip, res_ratio,
+    year == 2018, tract_fips == "01001020200", zip == "36067"
+  ) %>%
     expect_equal(1)
-    get_value(tract_to_zip, res_ratio,
-              year == 2019, tract_fips == "01001020200", zip == "36067") %>% 
+  get_value(
+    tract_to_zip, res_ratio,
+    year == 2019, tract_fips == "01001020200", zip == "36067"
+  ) %>%
     expect_equal(1)
 })
 
