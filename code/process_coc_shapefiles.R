21975f66ce3744f20a37fcc73dbae38ac428c7a4
Eve Perry <eve.perry@ucsf.edu>
2021-06-09 17:32:32 -0700

    update docs

--- code/process_coc_shapefiles.R
+++ code/process_coc_shapefiles.R
@@ -1,4 +1,4 @@
-#' Build filename regular expression
+#' Build file extension regular expression
 #'
 #' Creates a regular expression used to filter shapefile in input_data
 #'
@@ -6,6 +6,7 @@
 #'   include in the regular expression.
 #'
 #' @return A character vector containing the regular expression.
+#' @keywords internal
 build_regex <- function(file_extensions = c("gdb", "shp")) {
   paste0("[.](", paste(file_extensions, collapse = "|"), ")$")
 }
@@ -20,6 +21,8 @@ build_regex <- function(file_extensions = c("gdb", "shp")) {
 #'   file/database
 #'
 #' @return The shapefile in a spatial data frame
+#' @seealso [simplify_shapefile()] for simplifying the shapefile to reduce its
+#'   complexity
 read_raw_coc_shapefile <- function(shapefile_path) {
   # try to parse a year from the shapefile path
   shapefile_year <- parse_number(shapefile_path)
@@ -38,11 +41,14 @@ read_raw_coc_shapefile <- function(shapefile_path) {
 #' projected CRS using [sf::st_transform()] since [rmapshaper::ms_simplify()]
 #' prefers a projected CRS.
 #'
-#' @param shapefile An sf object
+#' @param shapefile An sf object with a raw CoC shapefile from
+#'   [read_raw_coc_shapefile()]
 #' @param projected_crs The EPSG code for the projected CRS
 #' @param simplify_keep_pct The share of the shapefile to keep when simplifying
 #'
 #' @return The simplified sf object now in the projected CRS.
+#' @seealso [read_raw_coc_shapefile()] for reading in the raw shapefile
+#' @seealso [match_tract_to_coc()] for using the shapefile to match CoCs to tracts
 simplify_shapefile <- function(shapefile, projected_crs = 2163, simplify_keep_pct = 0.1) {
   # transform to the projected CRS
   st_transform(shapefile, crs = projected_crs) %>%
