13132dbfe824678a244715e27399e0685dc81724
Eve Perry <eve.perry@ucsf.edu>
2021-07-09 13:45:19 -0700

    match tracts & cocs without processing coc shapefile

--- code/coc_county_tract_crosswalk.R
+++ code/coc_county_tract_crosswalk.R
@@ -29,40 +29,7 @@ fetch_tract_data <- function(year, crs) {
     st_point_on_surface()
 }
 
-#' Trim tracts outside of CoC boundaries
-#'
-#' Not all Census tracts are in CoC boundaries, this separates out the tracts
-#' that are not in a CoC.
-#'
-#' @param tract_geodata An sf object with the tract data & boundaries
-#' @param dissolved_cocs An sf object with the internal boundaries dissolved
-#'   since we only care at this point if a tract is in *any* CoC
-#'
-#' @return A list with two sf objects: one with the tracts that are in a CoC and
-#'   one with the tracts that aren't in a CoC
-clip_tracts <- function(tract_geodata, dissolved_cocs) {
-  # grab the year of the tract data
-  tract_year = unique(tract_geodata$year)
-  # grab the year of the CoC data
-  coc_year = unique(dissolved_cocs$year)
-  # throw an error if the tract and CoC data are not from the same year
-  stopifnot("tracts and dissolved coc shapefile not from same year" = tract_year == coc_year)
-  
-  # trim to just the tracts that intersect with a CoC
-  tracts_in_cocs <- ms_clip(tract_geodata, dissolved_cocs)
-  # get the tracts not in a CoC by starting with all the tracts
-  tracts_not_in_cocs <- tract_geodata %>%
-    # then filter to just the ones not in the tracts_in_coc table
-    filter(!(tract_fips %in% pluck(tracts_in_cocs$tract_fips))) %>%
-    # remove the boundary data from these tracts. they aren't in a CoC so we
-    # don't need it anymore
-    st_drop_geometry()
-  
-  # return a list with both tables
-  return(list("tracts_in_cocs" = tracts_in_cocs, "tracts_not_in_cocs" = tracts_not_in_cocs))
-}
-
-#' @describeIn map_tracts_to_cocs Match Census Tracts to CoCs
+#' Match Census Tracts to CoCs
 #'
 #' Matches Census Tracts to CoCs based on the CoC the center of the Census Tract
 #' lies within.
@@ -72,58 +39,12 @@ clip_tracts <- function(tract_geodata, dissolved_cocs) {
 match_tract_to_coc <- function(tract_points, coc_boundaries) {
   # match up the tract centers and CoCs
   st_intersection(tract_points, coc_boundaries) %>%
-    # strip off the geodata now that we're done with it
-    st_drop_geometry()
-}
-
-#' @describeIn map_tracts_to_cocs Combine tables of tracts in and not in CoCs
-#'
-#' Takes the matched table of tracts in CoCs, joins it with the table of tracts
-#' not in CoCs and cleans up the variables.
-#'
-#' @param tracts_in_cocs A data frame with tracts matched to CoCs
-#' @param tracts_not_in_cocs A data frame with tracts not in CoCs
-recombine_tracts <- function(tracts_in_cocs, tracts_not_in_cocs) {
-  # start with the tracts that are in CoCs
-  tracts_in_cocs %>%
-    # combine with the tracts that aren't in CoCs
-    bind_rows(tracts_not_in_cocs) %>%
-    mutate(
-      # add a column for the tract's county FIPS code (the first 5 characters of
-      # the tract FIPS code)
-      county_fips = str_sub(tract_fips, 1, 5),
-      # add a column for the tract's state FIPS code (the first 2 characters of
-      # the tract FIPS code)
-      state_fips = str_sub(tract_fips, 1, 2)
-    ) %>%
-    # just keep these variables and give friendlier names for the coc_number and
-    # coc_name columns
-    select(year, tract_fips, state_fips, county_fips, coc_number = COCNUM, coc_name = COCNAME, tract_pop, tract_pop_in_poverty)
-}
-
-#' Match tracts to CoCs
-#'
-#' Calls functions that calculate the center points for tracts in CoCs, match
-#' them to the CoC the center point lies within, recombine the tracts in and not
-#' in CoCs, and does some light cleaning.
-#'
-#' @param clipped_tracts A list with two elements: `tracts_in_cocs` &
-#'   `tracts_not_in_cocs`
-#' @param coc_boundaries An sf object with CoC boundaries
-#'
-#' @return A clean data frame of all tracts, their tract population data, and
-#'   the corresponding CoC if there is one.
-map_tracts_to_cocs = function(clipped_tracts, coc_boundaries) {
-  # start with the list of tables of tracts in and not in CoCs
-  clipped_tracts %>% 
-    # grab just the one with tracts in CoCs
-    pluck("tracts_in_cocs") %>% 
-    # get their center points
-    st_point_on_surface() %>%
-    # match them to CoCs
-    match_tract_to_coc(coc_boundaries) %>% 
-    # combine them with the tracts not in CoCs
-    recombine_tracts(clipped_tracts$tracts_not_in_cocs)
+    # strip off the spatial now that we're done with it
+    st_drop_geometry() %>% 
+    # just need the coc info
+    select(tract_fips, coc_number, coc_name) %>% 
+    # tracts that aren't in a coc get dropped above so add them back in
+    right_join(st_drop_geometry(tract_points))
 }
 
 #' Creates the tract to CoC crosswalk
@@ -139,11 +60,6 @@ map_tracts_to_cocs = function(clipped_tracts, coc_boundaries) {
 build_tract_crosswalk <- function(recombined_tracts) {
   # start with the tract/CoC table
   recombined_tracts %>%
-    # join on state names from the tidycensus FIPS code table
-    left_join(
-      distinct(tidycensus::fips_codes, state_code, state_name),
-      by = c("state_fips" = "state_code")
-    ) %>%
     # calculate yearly CoC level stats by summing over the tracts in each CoC 
     group_by(coc_number, year) %>%
     mutate(
@@ -154,9 +70,7 @@ build_tract_crosswalk <- function(recombined_tracts) {
       # make sure these variables are NA for tracts that aren't in a CoC (where
       # coc_number is NA)
       across(c(coc_pop, coc_poverty_pop, pct_coc_pop_from_tract, pct_coc_poverty_pop_from_tract), ~ if_else(is.na(coc_number), NA_real_, .x))
-    ) %>%
-    # move the state name column after the state_fips column
-    relocate(state_name, .after = state_fips) %>% 
+    ) %>% 
     # remove grouping for when it's used later
     ungroup()
 }
@@ -200,25 +114,6 @@ build_county_crosswalk <- function(tract_crosswalk) {
     filter(!is.na(coc_number)) %>% 
     # remove grouping for when it's used later
     ungroup()
-}
-
-#' Write the crosswalk to CSV
-#' 
-#' Creates a CSV flat text file with the crosswalk data.
-#'
-#' @param crosswalk A crosswalk data frame
-#' @param type A string with the type of crosswalk. Either `"tract"` or `"county"`.
-#' @param output_directory A path to the directory to save the CSV
-#'
-#' @return Returns the CSV path so [targets] can track it
-write_crosswalk <- function(crosswalk, type, output_directory) {
-  filename <- paste(type, "coc_crosswalk", sep = "_")
-  
-  crosswalk_path <- path(output_directory, filename, ext = "csv")
-  write_csv(crosswalk, crosswalk_path)
-  
-  # return the CSV path so targets can track it
-  return(crosswalk_path)
 }
 
 #' CoC populations and poverty rates
