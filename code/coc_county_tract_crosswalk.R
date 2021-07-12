77b526af2b1a2aa1b5da8b9d870bdf0be665f076
Eve Perry <eve.perry@ucsf.edu>
2021-07-12 10:46:35 -0700

    new shapefile processing method

--- code/coc_county_tract_crosswalk.R
+++ code/coc_county_tract_crosswalk.R
@@ -24,104 +24,27 @@ fetch_tract_data <- function(year, crs) {
     # clarify the tract FIPS code column name
     rename(tract_fips = fips) %>%
     # change the CRS for the tract boundaries
-    st_transform(crs)
+    st_transform(crs) %>% 
+    # all we need is the center point of the tract
+    st_point_on_surface()
 }
 
-#' Trim tracts outside of CoC boundaries
-#'
-#' Not all Census tracts are in CoC boundaries, this separates out the tracts
-#' that are not in a CoC.
-#'
-#' @param tract_geodata An sf object with the tract data & boundaries
-#' @param dissolved_cocs An sf object with the internal boundaries dissolved
-#'   since we only care at this point if a tract is in *any* CoC
-#'
-#' @return A list with two sf objects: one with the tracts that are in a CoC and
-#'   one with the tracts that aren't in a CoC
-clip_tracts <- function(tract_geodata, dissolved_cocs) {
-  # grab the year of the tract data
-  tract_year = unique(tract_geodata$year)
-  # grab the year of the CoC data
-  coc_year = unique(dissolved_cocs$year)
-  # throw an error if the tract and CoC data are not from the same year
-  stopifnot("tracts and dissolved coc shapefile not from same year" = tract_year == coc_year)
-  
-  # trim to just the tracts that intersect with a CoC
-  tracts_in_cocs <- ms_clip(tract_geodata, dissolved_cocs)
-  # get the tracts not in a CoC by starting with all the tracts
-  tracts_not_in_cocs <- tract_geodata %>%
-    # then filter to just the ones not in the tracts_in_coc table
-    filter(!(tract_fips %in% pluck(tracts_in_cocs$tract_fips))) %>%
-    # remove the boundary data from these tracts. they aren't in a CoC so we
-    # don't need it anymore
-    st_drop_geometry()
-  
-  # return a list with both tables
-  return(list("tracts_in_cocs" = tracts_in_cocs, "tracts_not_in_cocs" = tracts_not_in_cocs))
-}
-
-#' @describeIn map_tracts_to_cocs Match Census Tracts to CoCs
+#' Match Census Tracts to CoCs
 #'
 #' Matches Census Tracts to CoCs based on the CoC the center of the Census Tract
 #' lies within.
 #'
 #' @param tract_points sf object with the center points of the Census Tracts
 #' @param coc_boundaries sf object with the CoC boundaries
-match_tract_to_coc <- function(tract_points, coc_boundaries) {
+match_tracts_to_cocs <- function(tract_points, coc_boundaries) {
   # match up the tract centers and CoCs
   st_intersection(tract_points, coc_boundaries) %>%
-    # strip off the geodata now that we're done with it
-    st_drop_geometry()
-}
-
-#' @describeIn map_tracts_to_cocs Combine tables of tracts in and not in CoCs
-#'
-#' Takes the matched table of tracts in CoCs, joins it with the table of tracts
-#' not in CoCs and cleans up the variables.
-#'
-#' @param tracts_in_cocs A data frame with tracts matched to CoCs
-#' @param tracts_not_in_cocs A data frame with tracts not in CoCs
-recombine_tracts <- function(tracts_in_cocs, tracts_not_in_cocs) {
-  # start with the tracts that are in CoCs
-  tracts_in_cocs %>%
-    # combine with the tracts that aren't in CoCs
-    bind_rows(tracts_not_in_cocs) %>%
-    mutate(
-      # add a column for the tract's county FIPS code (the first 5 characters of
-      # the tract FIPS code)
-      county_fips = str_sub(tract_fips, 1, 5),
-      # add a column for the tract's state FIPS code (the first 2 characters of
-      # the tract FIPS code)
-      state_fips = str_sub(tract_fips, 1, 2)
-    ) %>%
-    # just keep these variables and give friendlier names for the coc_number and
-    # coc_name columns
-    select(year, tract_fips, state_fips, county_fips, coc_number = COCNUM, coc_name = COCNAME, tract_pop, tract_pop_in_poverty)
-}
-
-#' Match tracts to CoCs
-#'
-#' Calls functions that calculate the center points for tracts in CoCs, match
-#' them to the CoC the center point lies within, recombine the tracts in and not
-#' in CoCs, and does some light cleaning.
-#'
-#' @param clipped_tracts A list with two elements: `tracts_in_cocs` &
-#'   `tracts_not_in_cocs`
-#' @param coc_boundaries An sf object with CoC boundaries
-#'
-#' @return A clean data frame of all tracts, their tract population data, and
-#'   the corresponding CoC if there is one.
-map_tracts_to_cocs = function(clipped_tracts, coc_boundaries) {
-  # start with the list of tables of tracts in and not in CoCs
-  clipped_tracts %>% 
-    # grab just the one with tracts in CoCs
-    pluck("tracts_in_cocs") %>% 
-    # get their center points
-    st_point_on_surface() %>%
-    # match them to CoCs
-    match_tract_to_coc(coc_boundaries) %>% 
-    # combine them with the tracts not in CoCs
-    recombine_tracts(clipped_tracts$tracts_not_in_cocs)
+    # strip off the spatial now that we're done with it
+    st_drop_geometry() %>% 
+    # just need the coc info
+    select(tract_fips, coc_number, coc_name) %>% 
+    # tracts that aren't in a coc get dropped above so add them back in
+    right_join(st_drop_geometry(tract_points))
 }
 
 #' Creates the tract to CoC crosswalk
@@ -137,11 +60,6 @@ map_tracts_to_cocs = function(clipped_tracts, coc_boundaries) {
 build_tract_crosswalk <- function(recombined_tracts) {
   # start with the tract/CoC table
   recombined_tracts %>%
-    # join on state names from the tidycensus FIPS code table
-    left_join(
-      distinct(tidycensus::fips_codes, state_code, state_name),
-      by = c("state_fips" = "state_code")
-    ) %>%
     # calculate yearly CoC level stats by summing over the tracts in each CoC 
     group_by(coc_number, year) %>%
     mutate(
@@ -153,8 +71,6 @@ build_tract_crosswalk <- function(recombined_tracts) {
       # coc_number is NA)
       across(c(coc_pop, coc_poverty_pop, pct_coc_pop_from_tract, pct_coc_poverty_pop_from_tract), ~ if_else(is.na(coc_number), NA_real_, .x))
     ) %>%
-    # move the state name column after the state_fips column
-    relocate(state_name, .after = state_fips) %>% 
     # remove grouping for when it's used later
     ungroup()
 }
@@ -177,7 +93,7 @@ build_county_crosswalk <- function(tract_crosswalk) {
     group_by(county_fips, coc_number, year) %>%
     # grab the first value of these label columns that are the same for every
     # tract in a given CoC
-    summarise(across(c(state_fips, state_name, coc_name, coc_pop, coc_poverty_pop), first),
+    summarise(across(c(coc_name, coc_pop, coc_poverty_pop), first),
       county_pop_in_coc = sum(tract_pop),
       county_poverty_pop_in_coc = sum(tract_pop_in_poverty, na.rm = TRUE)
     ) %>%
--- code/process_coc_shapefiles.R
+++ code/process_coc_shapefiles.R
@@ -11,6 +11,77 @@ build_regex <- function(file_extensions = c("gdb", "shp")) {
   paste0("[.](", paste(file_extensions, collapse = "|"), ")$")
 }
 
+get_shapefiles <- function(year, raw_coc_shapefiles, crs) {
+  if (year < 2013) {
+    raw = get_pre_2013_shapefiles(year)
+  } else {
+    raw = keep(raw_coc_shapefiles, ~ str_detect(.x, as.character(year))) %>% 
+      read_raw_coc_shapefile()
+  }
+  
+  raw %>% 
+    st_transform(crs = crs)
+}
+
+#' Fetches pre-2013 CoC shapefiles
+#'
+#' Before 2013, HUD released individual shapefiles for each CoC instead of one
+#' national file. This downloads the individual shapefiles and combines them.
+#'
+#' @param year A numeric with the year of the shapefiles.
+#'
+#' @return A spatial data frame.
+get_pre_2013_shapefiles <- function(year) {
+  # augment the built-in vector of state names & abbreviations with DC
+  state_name <- c(state.name, "District of Columbia") 
+  state_abb <- c(state.abb, "DC")
+  
+  # iterate over the states and download the shapefiles
+  map2_dfr(state_abb, state_name, download_shapefile, year)
+}
+
+#' Download a state's CoC shapefiles
+#'
+#' Downloads the CoC shapefiles for a state in the given year.
+#'
+#' @param state_abb A character vector with two letter state abbreviations.
+#' @param state_name A character vector with state names.
+#' @param year A numeric with the year of the shapefiles.
+#' @param td A directory where the shapefiles will be unzipped. Defaults to the
+#'   session temporary directory.
+#'
+#' @return A spatial data frame.
+#' @seealso [get_pre_2013_coc_shapefiles()] for the main function
+#' @keywords internal
+download_shapefile <- function(state_abb, state_name, year, td = tempdir()) {
+  # paste together the url for the zip file with the state's shapefiles
+  url <- paste0("https://files.hudexchange.info/reports/published/CoC_GIS_State_Shapefile_", state_abb, "_", year, ".zip")
+  
+  # download the zipfile to a tempfile
+  curl::curl_download(url, file_temp()) %>%
+    # extract the contents of the zipfile into the temp directory
+    unzip(exdir = td)
+  
+  # swap spaces for underscores in the state name
+  state_name <- str_replace_all(state_name, " ", "_")
+  
+  # find the directories with the shapefiles in them
+  shapefile_dirs <- dir_ls(
+    # look for paths in the temp directory with the state's name
+    path(td, state_name),
+    # we don't want the metadata directory, so filter on that 
+    glob = "*metadata*",
+    # and take the paths that don't have metadata in them
+    invert = TRUE
+  )
+  
+  # iterate over the directories and read in the shapefile and tack on the year
+  map_dfr(shapefile_dirs, function(file) {
+    st_read(file, quiet = TRUE) %>%
+      mutate(year = year) %>% 
+      select(coc_number = COCNUM, coc_name = COCNAME, year, geometry)
+  })
+}
 
 #' Read in original CoC shapefile
 #'
@@ -29,29 +100,8 @@ read_raw_coc_shapefile <- function(shapefile_path) {
   # throw an error if we don't have a 4 digit number for the year
   stopifnot("coc shapefile year not 4 digits" = nchar(as.character(shapefile_year)) == 4)
   # read in the shapefile
-  st_read(shapefile_path) %>%
+  st_read(shapefile_path, quiet = TRUE) %>%
+    select(coc_number = COCNUM, coc_name = COCNAME) %>% 
     # add a column with the year
     mutate(year = shapefile_year)
-}
-
-
-#' Simplify shapefile and set projected CRS
-#'
-#' Simplifies a shapefile using [rmapshaper::ms_simplify()] and also sets a
-#' projected CRS using [sf::st_transform()] since [rmapshaper::ms_simplify()]
-#' prefers a projected CRS.
-#'
-#' @param shapefile An sf object with a raw CoC shapefile from
-#'   [read_raw_coc_shapefile()]
-#' @param projected_crs The EPSG code for the projected CRS
-#' @param simplify_keep_pct The share of the shapefile to keep when simplifying
-#'
-#' @return The simplified sf object now in the projected CRS.
-#' @seealso [read_raw_coc_shapefile()] for reading in the raw shapefile
-#' @seealso [match_tract_to_coc()] for using the shapefile to match CoCs to tracts
-simplify_shapefile <- function(shapefile, projected_crs = 2163, simplify_keep_pct = 0.1) {
-  # transform to the projected CRS
-  st_transform(shapefile, crs = projected_crs) %>%
-    # simplify the spatial object
-    ms_simplify(keep = simplify_keep_pct)
 }
